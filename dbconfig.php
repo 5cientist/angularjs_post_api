7606b779f7e5266c385a0c7149870e7af63c3348
Hanshir <hanshirlukku@gmail.com>
2020-07-07 19:36:25 +0530

    added index and dbconfig

---
+++ dbconfig.php
@@ -0,0 +1,7 @@
+<?php
+define ('DB_USER', "admin");
+define ('DB_PASSWORD', "admin");
+define ('DB_DATABASE', "angularjspost");
+define ('DB_HOST', "localhost");
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+?>
