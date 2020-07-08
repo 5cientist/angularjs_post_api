7fc665d3cce1f38caf2979553bac4c17a9151f72
Hanshir <hanshirlukku@gmail.com>
2020-07-08 07:09:35 +0530

    modified still error

--- api/insert_data.php
+++ api/insert_data.php
@@ -1,12 +1,16 @@
 <?php
 require '../dbconfig.php';
 $conn = mysqli_connect("localhost", "admin", "admin", "angularjspost");
-$info = json_decode(file_get_contents("php://input"));
+$info = (array)json_decode(file_get_contents("php://input"));
+// $info = json_decode(file_get_contents("php://input"));
 if(cont($info) > 0)
 {
-    $name = mysqli_real_escape_string($conn, $info->name);
-    $email = mysqli_real_escape_string($conn, $info->email);
-    $age = mysqli_real_escape_string($conn, $info->age);
+    // $name = mysqli_real_escape_string($conn, $info->name);
+    // $email = mysqli_real_escape_string($conn, $info->email);
+    // $age = mysqli_real_escape_string($conn, $info->age);
+    $name = mysqli_real_escape_string($conn, $info['name']);
+    $email = mysqli_real_escape_string($conn, $info['email']);
+    $age = mysqli_real_escape_string($conn, $info['age']);
     $query = "INSERT INTO insertdata(name, email, age) VALUES ('$name', '$email', '$age')";
     if(mysqli_query($conn, $query))
     {
--- app/route.js
+++ app/route.js
@@ -1,13 +1,15 @@
-var app = angular.module("insert",[]);
+var app = angular.module("insert",["ngRoute"]);
 app.controller("controller", function($scope, $http){
 $scope.insert = function(){
     $http.post(
-        "insert_data.php",
+        "api/insert_data.php",
         {'name':$scope.name, 'email':$scope.email, 'age':$scope.age}
     ).success(function(data){
+        alert(data);
         $scope.name = null;
         $scope.email = null;
         $scope.age = null ;
+        $scope.displayData()
     });
 
 
